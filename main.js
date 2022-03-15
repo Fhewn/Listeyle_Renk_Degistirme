f3dbf51442f08e65ad7c58bb3f0bc305ad347abc
Fhewn <76724264+Fhewn@users.noreply.github.com>
2022-03-15 19:26:55 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function rnk(a) {
+    if (a == 1) {
+        document.bgColor = "red";
+    } else if (a == 2) {
+        document.bgColor = "blue";
+    } else if (a == 3) {
+        document.bgColor = "yellow";
+    } else if (a == 4) {
+        document.bgColor = "purple";
+    } else if (a == 5) {
+        document.bgColor = "black";
+    } else if (a == 6) {
+        document.bgColor = "gray";
+    }
+
+}
